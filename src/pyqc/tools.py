1133c07c3e68b3f46d4ac2fdf3391558fc384c37
Sen Wang <wangsen992@gmail.com>
2020-03-23 20:36:02 +0800

    initiate tools package for specific functions to employ for use cases

---
+++ src/pyqc/tools.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import xarray as xr
+import matplotlib.pyplot as plt
+import ipdb as debugger
+
+def summarize_qc_resamples(input_df, verbose=False, **resample_kwargs):
+
+    time_list = list()
+    data_list = list()
+
+    for time, df in input_df.resample(**resample_kwargs):
+        if verbose == True:
+            print("Currently working on: {}".format(time))
+        time_list.append(time)
+        df_stats = df.qc.describe()
+        data_list.append(df_stats.values)
+    else:
+        measures = df_stats.index.to_list()
+        variables = df.columns.to_list()
+
+    attrs = resample_kwargs
+
+    return xr.DataArray(np.dstack(data_list),
+                        coords = [measures, variables, time_list],
+                        dims   = ['measure','variable','time'],
+                        name   = "qc_summary",
+                        attrs  = attrs)
